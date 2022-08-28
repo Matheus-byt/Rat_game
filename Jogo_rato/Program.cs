acb6fb67582e06e848b702b504ce2c97a055bfda
Matheus-byt <mmacedo41@hotmail.com>
2022-08-28 00:10:53 -0300

    Firts Commit

---
+++ Jogo_rato/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace Jogo_rato
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[,] jogo = new int[5, 10];
+
+            Random Randonizador = new Random();
+            int linebreaker = 0;
+
+
+            for (int i = 0; i < jogo.GetLength(0); i++)
+            {
+                for(int j = 0; j < jogo.GetLength(0); j++)
+                {
+                    jogo[i, j] = Randonizador.Next(2);
+                }      
+            }
+
+
+            jogo[0, 0] = 2;
+            jogo[2, 3] = 3;
+
+            foreach (int block in jogo)
+            {
+                if (linebreaker == 10)
+                {
+                    Console.WriteLine("\n");
+                    linebreaker = 0;
+
+
+                }
+
+
+
+                switch (block)
+                {
+                    case 0:
+                        Console.Write($"[   {block}  ]");
+                        break;
+                    case 1:
+                        Console.Write($"[   {block}  ]");
+                        break;
+                    case 2:
+                        Console.Write($"[ RATO ]");
+                        break;
+                    case 3:
+                        Console.Write($"[QUEIJO]");
+                        break;
+                        Console.WriteLine("");
+
+                }
+
+                linebreaker++;
+
+                
+            }
+            Console.WriteLine("");
+            Console.WriteLine("\n-----------------------------------[SAIDA]------------------------------------------\n");
+
+        }
+    }
+
+}
